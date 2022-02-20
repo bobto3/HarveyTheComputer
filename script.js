e3bdb434a14d1dbb0589c527b20c03e078198192
Robert Anthony <rwanthony3@gmail.com>
2022-02-19 20:09:06 -0800

    added external script file

---
+++ script.js
@@ -0,0 +1,23 @@
+const jim = new Audio();
+    jim.src = "Assets/Sound/Hi, Jim.mp3";
+        
+const sucks = new Audio();
+    sucks.src = "Assets/Sound/Jim sucks.mp3";
+
+const pam = new Audio();
+    pam.src = "Assets/Sound/Pam, you look very hot today.mp3";
+
+const horny = new Audio();
+    horny.src = "Assets/Sound/Me so horny, me love you long tim.mp3";
+
+const time = new Audio();
+    time.src = "Assets/Sound/Long time, me lobe yoy long time.mp3";
+
+const ruined = new Audio();
+    ruined.src = "Assets/Sound/You ruined a funny joke you.mp3";
+
+const outOf = new Audio();
+    outOf.src = "Assets/Sound/Get out of my offive.mp3";
+
+const boobz = new Audio();
+    boobz.src = "Assets/Sound/Boobz.mp3";
